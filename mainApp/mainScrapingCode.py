42c532f601a2b1fb06ba500edb8606dc637765b3
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-02-19 17:47:55 +0530

    added getsubjects func

--- mainApp/mainScrapingCode.py
+++ mainApp/mainScrapingCode.py
@@ -1,25 +1,40 @@
 #imports
 from selenium import webdriver
 import os
-from mainApp.resultsetcustom import getrs
+# from mainApp.resultsetcustom import getrs
+from resultsetcustom import getrs
 
 #Setting up Chrome options
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])#to remove the "Chrome is being controlled by automated test software" notification
 chrome_options.add_argument("--headless")#To make The Browser not appear / Headless Chrome
-
 currentWorkingDirectory=os.path.dirname(os.path.abspath(__file__))
 chromeDriverUrl=os.path.join(currentWorkingDirectory,"chromedriver_win32")#use join function so that it works in any OS
 chromeDriverUrl=os.path.join(chromeDriverUrl,"chromedriver.exe")
-
 driver = webdriver.Chrome(chromeDriverUrl,options=chrome_options)
+
+def getdetails(tabletag):
+    print("reached the function")
+    allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
+    # print(len(tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")))
+
+def getsubjectslessthanFive(tabletag,sem):
+    semdict={1:"I",2:"II",3:"III",4:"IV"}
+    allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
+    subList=[]
+    for trs in range(1,10):
+        subList.append(allTRsinTable[trs].find_elements_by_xpath("*")[0].text)
+        subList.append(allTRsinTable[trs].find_elements_by_xpath("*")[1].text)
+
+    semInRoman=semdict[sem]
+    # print(subList)
 
 def getSemRes(usnYear,semester,numberOfStudents):
     #input
-
     #usnYear=int(input("Enter the year:"))
     #semester=int(input("Enter the semester:"))
     #numberOfStudents=int(input("Enter the number of students:"))
+
 
     rs=getrs(usnYear,semester)
 
@@ -28,7 +43,6 @@ def getSemRes(usnYear,semester,numberOfStudents):
     urlPart2="/sem-"+str(semester)+"/rs-"+str(rs)+"?cbse=1"
     resultString=""
     for num in range(1,(numberOfStudents+1)):
-
         if num<10:
             url=urlPart1+"cs00"+str(num)+urlPart2
         elif num<100:
@@ -37,6 +51,8 @@ def getSemRes(usnYear,semester,numberOfStudents):
             url=urlPart1+"cs"+str(num)+urlPart2
 
         driver.get(url)
+        if num==1 and semester<5:
+            getsubjectslessthanFive(driver.find_element_by_class_name("table"),semester)
         try:
             #to get the name and USN
             usnAndNameDiv=driver.find_element_by_class_name("student_details").find_elements_by_xpath("*")
@@ -50,12 +66,16 @@ def getSemRes(usnYear,semester,numberOfStudents):
             #To get All Subject Marks
             #print(driver.find_element_by_class_name("table").text)
             resultString+=(driver.find_element_by_class_name("table").text)+"\n"
+            getdetails(driver.find_element_by_class_name("table"))
+            # getelements()
 
         except:
-            #print("no data available for "+str(num))
+            print("no data available for "+str(num))
             resultString+="no data available for "+str(num)+"\n"
         #print("-------------------------------------------------------------------")
         resultString+="-------------------------------------------------------------------\n"
 
     driver.close
     return resultString
+
+print(getSemRes(17,4,1))
