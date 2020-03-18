59b61829b4d23382334264a3a93e614e50eb4271
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-03-18 21:12:01 +0530

    Added thirdyrears functons (Excel)

--- mainApp/mainScrapingCode.py
+++ mainApp/mainScrapingCode.py
@@ -18,33 +18,24 @@ chromeDriverUrl=os.path.join(chromeDriverUrl,"chromedriver.exe")
 driver = webdriver.Chrome(chromeDriverUrl,options=chrome_options)
 
 def getdetailsYearTwo(tabletag,sheet,num):
-    # print("reached the function")
     allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-    # print(len(tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")))
     styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
                              "align:vertical center, horizontal center;")
     marksList=[]
     colVal=3
-    for trs in range(1,10):
+    for trs in range(1,len(allTRsinTable)-1):
         sheet.write(num+1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
         sheet.write(num+1, colVal+1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
         sheet.write(num+1, colVal+2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
         sheet.write(num+1, colVal+3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
         colVal+=7
-        # marksList.append(allTRsinTable[trs].find_elements_by_xpath("*")[2].text)
-        # marksList.append(allTRsinTable[trs].find_elements_by_xpath("*")[3].text)
-        # marksList.append(allTRsinTable[trs].find_elements_by_xpath("*")[4].text)
-        # marksList.append(allTRsinTable[trs].find_elements_by_xpath("*")[8].text)
     sheet.write(num+1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-    # totalM=allTRsinTable[10].find_elements_by_xpath("*")[1].text
-
 
 def getSubjectsYearTwoHeadings(tabletag,semester,sheet):
 
     allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
     subList=[]
-    for trs in range(1,10):
-        # subList.append(allTRsinTable[trs].find_elements_by_xpath("*")[0].text)
+    for trs in range(1,len(allTRsinTable)-1):
         subList.append(allTRsinTable[trs].find_elements_by_xpath("*")[1].text)
 
     styleTNR12 = xlwt.easyxf("font: name Times New Roman, bold 1,height 240;"
@@ -71,16 +62,63 @@ def getSubjectsYearTwoHeadings(tabletag,semester,sheet):
         colVal+=7
     sheet.write_merge(0, 1, colVal, colVal, "Total", styleC12)
 
+def yearThree(tabletag,sheet,y3Sub,num,y3total):
+    styleTNR12 = xlwt.easyxf("font: name Times New Roman, bold 1,height 240;"
+                             "align:vertical center, horizontal center;")
+    styleC12 = xlwt.easyxf("font: name Calibri, bold 1,height 220;"
+                           "align:vertical center, horizontal center")#height 20 means 1 in excel
+    styleC11Yellow = xlwt.easyxf(#"pattern: pattern solid,back_color red;"
+                            "font: name Calibri, bold 1,height 220;"
+                             "align:vertical center, horizontal center;")
+    allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
+    for trs in range(1,len(allTRsinTable)-1):
+        cSub=allTRsinTable[trs].find_elements_by_xpath("*")[1].text
+        if cSub in y3Sub:
+            ind=y3Sub.index(cSub)
+            sheet.write(num+1, ind*7+3, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
+        else:
+            #add new subject to the list of subjects
+            y3Sub.append(cSub)
+            ind=y3Sub.index(cSub)
+            #add the new subject as a heading in the excel sheet
+            sheet.write_merge(0, 0, ind*7+3, ind*7+6, cSub, styleC12)
+            sheet.write(1, ind*7+3, 'Int', styleC12)
+            sheet.write(1, ind*7+3+1, 'Ext', styleC12)
+            sheet.write(1, ind*7+3+2, 'Total', styleC12)
+            sheet.write(1, ind*7+3+3, 'Result', styleC12)
+            sheet.write_merge(0, 0, ind*7+3+4, ind*7+3+6, "Revaluvation", styleC11Yellow)
+            sheet.write(1, ind*7+3+4, 'Ext',styleC11Yellow)
+            sheet.write(1, ind*7+3+5, 'Total',styleC11Yellow)
+            sheet.write(1, ind*7+3+6, 'Result',styleC11Yellow)
+            #add the current students marks into the excel add_sheet
+            sheet.write(num+1, ind*7+3, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
+            sheet.write(num+1, ind*7+3+3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
+    y3total.append(allTRsinTable[-1].find_elements_by_xpath("*")[1].text)
+
 def getSemRes(usnYear,semester,numberOfStudents):
+    styleC12 = xlwt.easyxf("font: name Calibri, bold 1,height 220;"
+                           "align:vertical center, horizontal center")
     styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
                              "align:vertical center, horizontal center;")
-    semdict={1:"I",2:"II",3:"III",4:"IV"}
+    styleTNR12B = xlwt.easyxf("font: name Times New Roman, bold 1,height 240;"
+                             "align:vertical center, horizontal center;")
+    semdict={1:"I",2:"II",3:"III",4:"IV",5:"V",6:"VI",7:"VII",8:"VIII"}
     sheet = workbook.add_sheet(str(semdict[semester])+" Sem")
     rs=getrs(usnYear,semester)
     #initialize url
     urlPart1="https://www.vtu4u.com/result/4so"+str(usnYear)
     urlPart2="/sem-"+str(semester)+"/rs-"+str(rs)+"?cbse=1"
     resultString=""
+    if semester==5 or semester==6:
+        sheet.write_merge(0, 1, 1, 1, 'USN', styleTNR12B)   #(top_row, bottom_row, left_column, right_column,content,style)
+        sheet.write_merge(0, 1, 2, 2, 'NAME', styleTNR12B)
+        y3Sub=[]
+        y3total=[]
     for num in range(1,(numberOfStudents+1)):
         if num<10:
             url=urlPart1+"cs00"+str(num)+urlPart2
@@ -107,8 +145,10 @@ def getSemRes(usnYear,semester,numberOfStudents):
             '''
                 Entering Details into Excel
             '''
-            getdetailsYearTwo(driver.find_element_by_class_name("table"),sheet,num)
-
+            if semester==3 or semester==4:
+                getdetailsYearTwo(driver.find_element_by_class_name("table"),sheet,num)
+            if semester==5 or semester==6:
+                yearThree(driver.find_element_by_class_name("table"),sheet,y3Sub,num,y3total)
 
 
 
@@ -116,11 +156,18 @@ def getSemRes(usnYear,semester,numberOfStudents):
             # print("no data available for "+str(num))
             resultString+="no data available for "+str(num)+"\n"
             sheet.write(num+1,2,"no data available for "+str(num),styleTNR12)
+            if semester==5 or semester==6:
+                y3total.append("0")
         #print("-------------------------------------------------------------------")
         resultString+="-------------------------------------------------------------------\n"
 
+    if semester==5 or semester==6:
+        sheet.write_merge(0, 1, len(y3Sub)*7+3, len(y3Sub)*7+3, "Total", styleC12)
+        for m in range(0,len(y3total)):
+            sheet.write(m+2, len(y3Sub)*7+3, y3total[m], styleTNR12)
+
     driver.close
-    workbook.save("sample.xls")
+    workbook.save("4SO"+str(usnYear)+"CS-SEM-"+str(semester)+".xls")
     return resultString
 
 # print(getSemRes(17,4,1))
