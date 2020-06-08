9c8158473d1ca81bc0e6f2168aeebb59d5d7bfee
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-06-08 21:25:06 +0530

    bug fixes version : 1.1

--- mainApp/mainScrapingCode.py
+++ mainApp/mainScrapingCode.py
@@ -2,8 +2,12 @@
 from selenium import webdriver
 import os
 import xlwt
-from mainApp.resultsetcustom import getrs,getrs1
+from mainApp.resultsetcustom import getrs
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 # from resultsetcustom import getrs
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,110 +21,6 @@ currentWorkingDirectory=os.path.dirname(os.path.abspath(__file__))
 chromeDriverUrl=os.path.join(currentWorkingDirectory,"chromedriver_win32")#use join function so that it works in any OS
 chromeDriverUrl=os.path.join(chromeDriverUrl,"chromedriver.exe")
 driver = webdriver.Chrome(chromeDriverUrl,options=chrome_options)
-driver.maximize_window()
-driver.implicitly_wait(30)
-
-def getdetailsYearTwo(tabletag,sheet,num,url,semester):
-    if url.find("cs4")!=-1 and semester==4 :
-        allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
-                                 "align:vertical center, horizontal center;")
-        marksList=[]
-        colVal=3
-        for trs in range(1,len(allTRsinTable)-1):
-            sheet.write(num+1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
-            sheet.write(num+1, colVal+1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
-            sheet.write(num+1, colVal+2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
-            sheet.write(num+1, colVal+3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
-            colVal+=7
-        sheet.write(num+1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-    elif semester == 4:
-        allTRsinTable = tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
-                                 "align:vertical center, horizontal center;")
-        marksList = []
-        colVal = 10
-        for trs in range(1, len(allTRsinTable) - 1):
-            sheet.write(num + 1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
-            sheet.write(num + 1, colVal + 1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
-            sheet.write(num + 1, colVal + 2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
-            sheet.write(num + 1, colVal + 3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
-            colVal += 7
-        sheet.write(num + 1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-    elif url.find("cs4")!= -1 and semester == 3:
-        allTRsinTable = tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
-                                 "align:vertical center, horizontal center;")
-        marksList = []
-        colVal = 3
-        for trs in range(1, len(allTRsinTable) - 1):
-            sheet.write(num + 1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
-            sheet.write(num + 1, colVal + 1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
-            sheet.write(num + 1, colVal + 2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
-            sheet.write(num + 1, colVal + 3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
-            colVal += 7
-        sheet.write(num + 1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-    elif url.find("cs0")!=-1 or url.find("cs1")!=-1 and semester==3:
-        allTRsinTable = tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
-                                 "align:vertical center, horizontal center;")
-        marksList = []
-        colVal=3
-        sheet.write(num+1,colVal,allTRsinTable[1].find_elements_by_xpath("*")[2].text,styleTNR12)
-        sheet.write(num+1,colVal+1,allTRsinTable[1].find_elements_by_xpath("*")[3].text,styleTNR12)
-        sheet.write(num+1,colVal+2,allTRsinTable[1].find_elements_by_xpath("*")[4].text,styleTNR12)
-        sheet.write(num+1, colVal+3, allTRsinTable[1].find_elements_by_xpath("*")[8].text,styleTNR12)
-        colVal=17
-        for trs in range(2, len(allTRsinTable) - 1):
-            sheet.write(num + 1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
-            sheet.write(num + 1, colVal + 1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
-            sheet.write(num + 1, colVal + 2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
-            sheet.write(num + 1, colVal + 3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
-            colVal += 7
-        sheet.write(num + 1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-    else:
-        allTRsinTable = tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, height 240;"
-                                 "align:vertical center, horizontal center;")
-        marksList = []
-        colVal = 3
-        for trs in range(1, len(allTRsinTable) - 1):
-            sheet.write(num + 1, colVal, allTRsinTable[trs].find_elements_by_xpath("*")[2].text, styleTNR12)
-            sheet.write(num + 1, colVal + 1, allTRsinTable[trs].find_elements_by_xpath("*")[3].text, styleTNR12)
-            sheet.write(num + 1, colVal + 2, allTRsinTable[trs].find_elements_by_xpath("*")[4].text, styleTNR12)
-            sheet.write(num + 1, colVal + 3, allTRsinTable[trs].find_elements_by_xpath("*")[8].text, styleTNR12)
-            colVal += 7
-        sheet.write(num + 1, colVal, allTRsinTable[-1].find_elements_by_xpath("*")[1].text, styleTNR12)
-
-def getSubjectsYearTwoHeadings(tabletag,semester,sheet):
-        allTRsinTable=tabletag.find_element_by_tag_name("tbody").find_elements_by_xpath("*")
-        subList=[]
-        for trs in range(1,len(allTRsinTable)-1):
-            subList.append(allTRsinTable[trs].find_elements_by_xpath("*")[1].text)
-
-        styleTNR12 = xlwt.easyxf("font: name Times New Roman, bold 1,height 240;"
-                                 "align:vertical center, horizontal center;")
-        styleC12 = xlwt.easyxf("font: name Calibri, bold 1,height 220;"
-                               "align:vertical center, horizontal center")#height 20 means 1 in excel
-        styleC11Yellow = xlwt.easyxf(#"pattern: pattern solid,back_color red;"
-                                "font: name Calibri, bold 1,height 220;"
-                                 "align:vertical center, horizontal center;")
-
-        sheet.write_merge(0, 1, 1, 1, 'USN', styleTNR12)   #(top_row, bottom_row, left_column, right_column,content,style)
-        sheet.write_merge(0, 1, 2, 2, 'NAME', styleTNR12)
-        colVal=3
-        for index in range(0,len(subList)):
-            sheet.write_merge(0, 0, colVal, colVal+3, subList[index], styleC12)
-            sheet.write(1, colVal, 'Int', styleC12)
-            sheet.write(1, colVal+1, 'Ext', styleC12)
-            sheet.write(1, colVal+2, 'Total', styleC12)
-            sheet.write(1, colVal+3, 'Result', styleC12)
-            sheet.write_merge(0, 0, colVal+4, colVal+6, "Revaluvation", styleC11Yellow)
-            sheet.write(1, colVal+4, 'Ext',styleC11Yellow)
-            sheet.write(1, colVal+5, 'Total',styleC11Yellow)
-            sheet.write(1, colVal+6, 'Result',styleC11Yellow)
-            colVal+=7
-        sheet.write_merge(0, 1, colVal, colVal, "Total", styleC12)
 
 def yearThree(tabletag,sheet,y3Sub,num,y3total):
     styleTNR12 = xlwt.easyxf("font: name Times New Roman, bold 1,height 240;"
@@ -174,18 +74,6 @@ def getSemRes(usnYear,semester,numberOfStudents,numberOfDiplomas):
     urlPart3 = "https://www.vtu4u.com/result/4so"+str(usnYear)
     urlPart1 = "https://www.vtu4u.com/result/4so" + str(usnYear+1)
     resultString = ""
-    url1=urlPart1+"cs400"+urlPart2
-    driver.get(url1)
-    sleep(1)
-    if semester == 3 or semester == 4:
-        getSubjectsYearTwoHeadings(driver.find_element_by_class_name("table"), semester, sheet)
-    if semester==5 or semester==6:
-        sheet.write_merge(0, 1, 1, 1, 'USN', styleTNR12B)   #(top_row, bottom_row, left_column, right_column,content,style)
-        sheet.write_merge(0, 1, 2, 2, 'NAME', styleTNR12B)
-        y3Sub=[]
-        y3total=[]
-
-    num2=1
     for num in range(1,(numberOfStudents+1)):
         if num<10:
             url = urlPart3+"cs00"+str(num)+urlPart2
@@ -194,8 +82,14 @@ def getSemRes(usnYear,semester,numberOfStudents,numberOfDiplomas):
         else:
             url = urlPart3+"cs"+str(num)+urlPart2
         driver.get(url)
-
+        sleep(2)
+        print(url)
         try:
+            if num ==1:
+                    sheet.write_merge(0, 1, 1, 1, 'USN', styleTNR12B)   #(top_row, bottom_row, left_column, right_column,content,style)
+                    sheet.write_merge(0, 1, 2, 2, 'NAME', styleTNR12B)
+                    y3Sub=[]
+                    y3total=[]
             #to get the name and USN
             usnAndNameDiv=driver.find_element_by_class_name("student_details").find_elements_by_xpath("*")
             studentName=str(usnAndNameDiv[0].text)[11:] #slicing the string to remove unwanted conent / the 0th index contains the name
@@ -206,29 +100,20 @@ def getSemRes(usnYear,semester,numberOfStudents,numberOfDiplomas):
             sheet.write(num+1,2,studentName,styleTNR12)
             #To get All Subject Marks
             resultString+=(driver.find_element_by_class_name("table").text)+"\n"
-            # totalM=getdetails(driver.find_element_by_class_name("table"))
-
-            if semester==3 or semester==4:
-                getdetailsYearTwo(driver.find_element_by_class_name("table"),sheet,num,url,semester)
-            if semester==5 or semester==6:
-                yearThree(driver.find_element_by_class_name("table"),sheet,y3Sub,num,y3total)
+            yearThree(driver.find_element_by_class_name("table"),sheet,y3Sub,num,y3total)
         except:
-            # print("no data available for "+str(num))
             resultString+="no data available for "+str(num)+"\n"
             sheet.write(num+1,2,"no data available for "+str(num),styleTNR12)
-            if semester==5 or semester==6:
-                y3total.append("0")
-        #print("-------------------------------------------------------------------")
+            y3total.append("0")
         resultString+="-------------------------------------------------------------------\n"
 
-    rs = getrs1(usnYear+1, semester)
     for num in range(0,(numberOfDiplomas)):
         if num<10:
             url=urlPart1+"cs40"+str(num)+urlPart2
         else:
             url=urlPart1+"cs4"+str(num)+urlPart2
         driver.get(url)
-
+        sleep(2)
         try:
             #to get the name and USN
             usnAndNameDiv=driver.find_element_by_class_name("student_details").find_elements_by_xpath("*")
@@ -240,31 +125,20 @@ def getSemRes(usnYear,semester,numberOfStudents,numberOfDiplomas):
             sheet.write(numberOfStudents+num+2,2,studentName,styleTNR12)
             #To get All Subject Marks
             resultString+=(driver.find_element_by_class_name("table").text)+"\n"
-            # totalM=getdetails(driver.find_element_by_class_name("table"))
-
-            if semester==3 or semester==4:
-                getdetailsYearTwo(driver.find_element_by_class_name("table"),sheet,numberOfStudents+num+1,url,semester)
-            if semester==5 or semester==6:
-                yearThree(driver.find_element_by_class_name("table"),sheet,y3Sub,numberOfStudents+num+1,y3total)
+            yearThree(driver.find_element_by_class_name("table"),sheet,y3Sub,numberOfStudents+num+1,y3total)
 
 
 
         except:
-            # print("no data available for "+str(num))
             resultString+="no data available for "+str(num)+"\n"
             sheet.write(num+2,2,"no data available for "+str(num),styleTNR12)
-            if semester==5 or semester==6:
-                y3total.append("0")
-        #print("-------------------------------------------------------------------")
+            y3total.append("0")
         resultString+="-------------------------------------------------------------------\n"
 
-    if semester==5 or semester==6:
-        sheet.write_merge(0, 1, len(y3Sub)*7+3, len(y3Sub)*7+3, "Total", styleC12)
-        for m in range(0,len(y3total)):
-            sheet.write(m+2, len(y3Sub)*7+3, y3total[m], styleTNR12)
+    sheet.write_merge(0, 1, len(y3Sub)*7+3, len(y3Sub)*7+3, "Total", styleC12)
+    for m in range(0,len(y3total)):
+        sheet.write(m+2, len(y3Sub)*7+3, y3total[m], styleTNR12)
 
     driver.close
     workbook.save("4SO"+str(usnYear)+"CS-SEM-"+str(semester)+".xls")
     return resultString
-
-# print(getSemRes(17,4,1))
