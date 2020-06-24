f9fe8141bfc85cc316cf6d96effd10b4074e1980
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-06-25 04:45:01 +0530

    clean up on contacts.html
    removed unessesary static files

--- mainApp/SplitString.py
+++ mainApp/SplitString.py
@@ -32,26 +32,3 @@ def getdata(fullString):
             stud.append(subLst)
         studentdataList.append(stud)
     return studentdataList
-
-    #     if lst == ['']:
-    #         continue
-    #     studentData = lst[1][69:]
-    #     print(studentData)
-    #     import re
-    #     matches = re.finditer(' P|\D F',studentData)
-    #     matches_positions = [match.start() for match in matches]
-    #     print(matches_positions)
-    #     prev=0
-    #     stud=[]
-    #     for i in matches_positions:
-    #         subject=studentData[prev:i+3].strip()
-    #         prev=i+3
-    #         temp_subSplit=subject.split()
-    #         subLst=[]
-    #         subLst.append(temp_subSplit[0])
-    #         subLst.append(" ".join(temp_subSplit[1:-7]))
-    #         for colVals in range(len(temp_subSplit)-7,len(temp_subSplit)):
-    #             subLst.append(temp_subSplit[colVals])
-    #         stud.append(subLst)
-    #     studentdataList.append(stud)
-    # return studentdataList
