17e82f1b37e3164fd01beaa3710c9449cb60d0fc
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-06-27 01:23:20 +0530

    Update settings.py

--- MarksWebScraping/settings.py
+++ MarksWebScraping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')#'nz+_%!gp7==03*n*c#@$nivc#46u=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marksscraper.herokuapp.com']
 
 
 # Application definition
